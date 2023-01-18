ef835c9d8eae790181b49ddd4177fda65803bfff
zizodix <122798633+zizodix@users.noreply.github.com>
2023-01-18 10:36:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# This is a simple program that prints "Hello, World!" to the console
+
 print ("hello")
-# First, we define a function called "main"
+
 def main():
-    # Inside the main function, we use the print function to print the string "Hello, World!"
+
     print("Hello, World!")
 
-# Finally, we call the main function to run the program
+
 if __name__ == "__main__":
     main()
